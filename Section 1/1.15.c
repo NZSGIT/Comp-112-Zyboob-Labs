66bbd9d2e3e73bd327038e86cfcf04c8ab3ecd8f
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-18 18:22:23 -0700

    Create 1.15.c

---
+++ Section 1/1.15.c
@@ -0,0 +1,17 @@
+
+ /* 
+1.15 LAB: Formatted output: Hello World!
+Write a program that outputs "Hello World!" For ALL labs, end with newline (unless otherwise stated).
+*/
+
+/*----------------------------------------------------------------------------------------------------------*/
+//Code Below
+
+#include <stdio.h>
+
+int main(void) {
+
+   printf("Hello World!\n");
+
+   return 0;
+}
