ec50865f687d22cc330f31d9c5222d434789a161
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-18 18:24:29 -0700

    Create 1.19.c

---
+++ Section 1/1.19.c
@@ -0,0 +1,40 @@
+/*
+1.19 LAB: Warm up: Hello world
+This zyLab activity prepares a student for a full programming assignment. Warm up exercises are typically simpler and worth fewer points than a full programming assignment, and are well-suited for an in-person scheduled lab meeting or as self-practice.
+
+
+
+For each of the following steps, end the program's output with a newline.
+
+
+(1) Write a program that outputs the following. (1 pt)
+
+Hello world!
+
+(2) Update to output the following. (1 pt)
+
+Hello world!
+How are you? 
+
+(3) Finally, update to output the following. (1 pt)
+
+Hello world!
+How are you?
+   (I'm fine).
+   
+*/
+
+/*----------------------------------------------------------------------------------------------------------*/
+//Code Below
+
+
+#include <stdio.h>
+
+int main(void) {
+
+
+   printf("Hello world!\nHow are you?\n   (I'm fine).\n");
+   
+
+   return 0;
+}
