a71fd0778c1e2da041573a90d4f9b07e00a63986
NZSGIT <77271272+NZSGIT@users.noreply.github.com>
2021-05-18 18:23:03 -0700

    Create 1.17.c

---
+++ Section 1/1.17.c
@@ -0,0 +1,19 @@
+/*
+1.17 LAB: Formatted output: No parking sign
+Write a program that prints a formatted "No parking" sign as shown below. Note the first line has two leading spaces. For ALL labs, end with newline (unless otherwise stated).
+
+  NO PARKING
+2:00 - 6:00 a.m.
+*/ 
+
+/*----------------------------------------------------------------------------------------------------------*/
+//Code Below
+
+#include <stdio.h>
+
+int main(void) {
+
+  printf("  NO PARKING\n2:00 - 6:00 a.m.\n");
+
+   return 0;
+}
